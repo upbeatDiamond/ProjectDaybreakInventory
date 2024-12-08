e8338fbb10e4f01dfc0142f37c7002a88b40ff26
upbeatDiamond <upbeatDiamond@gmail.com>
2024-12-08 14:01:03 -0500

    Test item slot creation

--- inventory/inventory_list.gd
+++ inventory/inventory_list.gd
@@ -7,6 +7,12 @@ const INVENTORY_SLOT = preload("res://inventory/inventory_slot.tscn")
 
 ## 0-indexed
 var current_selected := 0
+var busy := false
+
+
+
+signal busy_ended
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,6 +22,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	if Input.is_action_pressed("make"):
+		_add_entry(str(randi_range(0, 1024)), randi_range(1, 64) )
+	if Input.is_action_pressed("kill"):
+		clear_entries()
+	
+	
 	pass
 
 
@@ -24,6 +37,10 @@ func _on_focus_entered():
 
 
 func clear_entries():
+	
+	for item in slot_list.get_children():
+		item.queue_free()
+	
 	pass
 
 
@@ -35,10 +52,54 @@ func set_entry(item, count:int):
 
 
 func _add_entry(item, count:int):
-	var entry = INVENTORY_SLOT.instantiate()
-	slot_list.add_child(entry)
 	
-	pass
+	## Mutex to reduce chance of list corruption
+	if busy:
+		await busy_ended
+	busy = true
+	
+	## Create the item slot
+	var entry = INVENTORY_SLOT.instantiate()
+	entry.item = item
+	entry.count = count
+	
+	## Resort all the slots to ensure that the entry is in the correct position
+	var sorted_nodes := slot_list.get_children().duplicate()
+	sorted_nodes.append(entry)
+	sorted_nodes.sort_custom(_sort_slots)
+	
+	for node in slot_list.get_children():
+		slot_list.remove_child(node)
+	
+	for node in sorted_nodes:
+		slot_list.add_child(node)
+	
+	## Have the neighbors properly assigned, so if the overall display fails, 
+	## the keyboard input still works.
+	for node in slot_list.get_children():
+		var node_index = slot_list.get_children().find(node)
+		if node_index > 0:
+			var neighbor = slot_list.get_children()[node_index-1]
+			node.focus_neighbor_top = neighbor.get_path()
+			neighbor.focus_neighbor_bottom = node.get_path()
+		
+		if node_index + 1 < slot_list.get_children().size():
+			var neighbor = slot_list.get_children()[node_index+1]
+			node.focus_neighbor_bottom = neighbor.get_path()
+			neighbor.focus_neighbor_top = node.get_path()
+	
+	busy = false
+	busy_ended.emit()
+
+
+# For descending order use > 0
+func _sort_slots(a:InventoryListItem, b:InventoryListItem):
+	if a.list_index < b.list_index:
+		return true
+	elif a.list_index > b.list_index:
+		return false
+	else:
+		return a.text < b.text
 
 
 func set_selection_index(index:int):
--- inventory/inventory_slot.gd
+++ inventory/inventory_slot.gd
@@ -3,9 +3,29 @@ class_name InventoryListItem
 
 signal selected( item:InventoryListItem )
 
-var count : int = 0
-var text : String = ""
+var count : int = 0 :
+	set(value):
+		count_label = str("x", value)
+		count = value
+		dirty = true
+	get:
+		if count_label != null:
+			return count_label.text.to_int()
+		return count
+
+var text : String = "" :
+	set(value):
+		name_label = str(value)
+		text = value
+		dirty = true
+	get:
+		if name_label != null:
+			return name_label.text
+		return text
+
+var dirty := true
 var item = "null"
+var list_index : int = 0
 
 @onready var count_label = $Count
 @onready var name_label = $HBoxContainer/Name
@@ -14,12 +34,33 @@ var item = "null"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pressed.connect(_on_pressed)
+	_update_labels()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if dirty:
+		_update_labels()
 	pass
+
+
+func _update_labels():
+	var clean = true
+	
+	## If the count didn't change, it's still dirty.
+	if count_label != null:
+		count_label.text = str(count)
+	else:
+		clean = false
+	
+	## If the name didn't change, it's still dirty.
+	if name_label != null:
+		name_label.text = str(item)
+	else:
+		clean = false
+	
+	dirty = not clean ## If anything failed, the slot remains dirty.
 
 
 func _on_pressed():
